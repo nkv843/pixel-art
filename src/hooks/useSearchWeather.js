2417bc356b5abfc27b02731fba9889bb196d128e
Kirill Nikitin <kirill.nikitin@stefanini.com>
2022-11-14 16:48:48 +0200

    feat: custom-hook

---
+++ src/hooks/useSearchWeather.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { geoapifyClient, sevenTimerClient } from '../api';
+import prettifyWeatherData from '../utils';
+
+const useSearchWeather = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [weathers, setWeathers] = useState([]);
+
+  const execute = async (location) => {
+    setLoading(true);
+    try {
+      const {
+        address, timezone, latitude, longitude,
+      } = await geoapifyClient.getDataByName(location);
+      const weatherData = await sevenTimerClient.getWeatherData(longitude, latitude);
+      const newWeather = {
+        ...weatherData,
+        id: Date.now(),
+        address,
+        timezone,
+      };
+      const formattedWeather = prettifyWeatherData(newWeather);
+      setWeathers([formattedWeather, ...weathers]);
+    } catch (e) {
+      setError(`${e.name}: ${e.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return [execute, { loading, error, weathers }];
+};
+
+export default useSearchWeather;

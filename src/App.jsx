bb3a3b2f3c1640c8ba3bfb97539b5e8f20c9564e
nkv843 <73536316+nkv843@users.noreply.github.com>
2023-03-12 23:28:05 +0200

    feat: context

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,100 @@
-import React from 'react';
-import SearchForm from './features/SearchForm';
-import PageHeader from './components/PageHeader';
-import WeatherList from './features/WeatherList';
-import classNames from './App.module.css';
-import useSearchWeather from './hooks/useSearchWeather';
-import ErrorMessage from './components/ErrorMessage';
+/* eslint-disable react/display-name */
+import React, { useState, createContext, useContext } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { HexAlphaColorPicker } from 'react-colorful';
+import PropTypes from 'prop-types';
+import {
+  Box,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  Select,
+  InputLabel,
+  CssBaseline,
+  Grid,
+} from '@mui/material';
+
+const theme = createTheme({});
+
+const GridCell = ({ side }) => {
+  const [externalColor, setExternalColor] = useState('#ffffff');
+
+  return (
+    <Grid
+      item
+      sx={{ backgroundColor: externalColor }}
+      xs={12 / side}
+      onClick={(color) => setExternalColor(color)}
+      border="1px solid #000000"
+      height="32px"
+    />
+  );
+};
+
+const MyProvider = ({ children }) => {
+  const MyContext = createContext();
+  const [color, setColor] = useState('#ffffff');
+
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <MyContext.Provider value={{ color, setColor }}>
+      {children}
+    </MyContext.Provider>
+  );
+};
+
+MyProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 const App = () => {
-  const [execute, { loading, error, weathers }] = useSearchWeather();
-  if (weathers.length) {
-    return (
-      <div className={classNames.app}>
-        <PageHeader loading={loading}>
-          <SearchForm onSearch={execute} />
-        </PageHeader>
-        {error && <ErrorMessage error={error} />}
-        <WeatherList weathers={weathers} />
-      </div>
-    );
-  }
+  const [color, setColor] = useState('#ffffff');
+  const [side, setSide] = React.useState(8);
+
+  const handleChange = (event) => setSide(event.target.value);
+
   return (
-    <div className={classNames.app}>
-      <PageHeader loading={loading}>
-        <SearchForm onSearch={execute} />
-      </PageHeader>
-      {error
-        ? <ErrorMessage error={error} />
-        : <h1 className={classNames.title}>Let&apos;s start explore</h1>}
-    </div>
+    <MyProvider>
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box sx={{ m: 2 }}>
+          <HexAlphaColorPicker color={color} onChange={setColor} />
+        </Box>
+        <Box sx={{ m: 2 }}>
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel>Side</InputLabel>
+            <Select
+              value={side}
+              label="Side"
+              onChange={handleChange}
+            >
+              <MenuItem value={8}>8</MenuItem>
+              <MenuItem value={16}>16</MenuItem>
+              <MenuItem value={32}>32</MenuItem>
+            </Select>
+            <FormHelperText>Please choose</FormHelperText>
+          </FormControl>
+        </Box>
+        <Box sx={{ m: 2 }}>
+          <Grid container>
+            {Array.from({ length: side * side }).map(() => (
+              <GridCell
+                key={Math.random()}
+                side={side}
+                color={color}
+              />
+            ))}
+          </Grid>
+        </Box>
+      </ThemeProvider>
+    </MyProvider>
   );
+};
+
+GridCell.propTypes = {
+  color: PropTypes.string.isRequired,
+  side: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import { sevenTimerClient, SevenTimerHttpClient } from './SevenTimerHttpClient';
-import { geoapifyClient, GeoapifyHttpClient } from './GeoapifyHttpClient';
-
-export { geoapifyClient, GeoapifyHttpClient };
-export { sevenTimerClient, SevenTimerHttpClient };
